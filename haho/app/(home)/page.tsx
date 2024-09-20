a450f27862cb0e2fc0e819e38586e649879b8400
Hadan2 <108774463+Hadan2@users.noreply.github.com>
2024-09-20 02:53:18 +0000

    09.20

--- haho/app/(home)/page.tsx
+++ haho/app/(home)/page.tsx
@@ -1,10 +1,15 @@
 import Navigation from "@/components/navigation";
-
+export const metadata = {
+  title: {
+    default: "home",
+    template: "%s"
+  }
+}
 
 export default function Home() {
   return (
     <div>
-      <Navigation/>
+      home!
     </div>
   );
 }
---
+++ haho/app/about-us/layout.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+
+
+export default function RootLayout({ children,}: 
+  {children: React.ReactNode}) {
+  return (
+     <div>
+        {children}
+        &copy; Next JS is great!
+     </div>
+        
+  );}
